3cf9eae658dbe7e2994ae0aaaafee133acdd0ec5
ayoline <ayo@outlook.com.br>
2022-04-04 13:04:12 -0300

    add meal_item.dart to organize categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
